0ad51dca38568f54e5c433972966ac0f79a2fdc3
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-04-29 15:52:40 +0300

    refactor base component

--- components/baseComponent.ts
+++ components/baseComponent.ts
@@ -1,4 +1,3 @@
-
 import { DetoxSelector } from "../types/detox";
 
 export class BaseComponent {
@@ -14,34 +13,5 @@ export class BaseComponent {
 
   async tap() {
     await this.element.tap();
-  }
-
-  async input(text: string) {
-    await this.element.replaceText(text);
-  }
-
-  async clearText() {
-    await this.element.clearText();
-  }
-
-  async visible() {
-    await expect(this.element).toBeVisible();
-  }
-
-  async notVisible() {
-    await expect(this.element).not.toBeVisible()
-  }
-
-  async exists() {
-    await expect(this.element).toExist();
-  }
-
-  async notExists() {
-    await expect(this.element).not.toExist();
-  }
-
-  async setDate(date: Date | string, format: string = 'ISO8601') {
-    const dateStr = typeof date === 'string' ? date : date.toISOString();
-    await this.element.setDatePickerDate(dateStr, format);
   }
 }
