293107b2e5900a4c6b3bf4a5def3693a846a6ec6
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-04-30 12:38:12 +0300

    refactored baseComponent class to fit propper ts structure

--- components/baseComponent.ts
+++ components/baseComponent.ts
@@ -7,20 +7,15 @@ export class BaseComponent {
     this.selector = selector;
   }
 
-  public get element() {
+  public getElement() {
     return element(this.selector);
   }
 
-  async tap() {
-    await this.element.tap();
+  public async tap() {
+    await this.getElement().tap();
   }
 
-  async multitap(amount: number) {
-    await this.element.multiTap(amount);
+  public async multitap(amount: number) {
+    await this.getElement().multiTap(amount);
   }
-
-  // should be in scrollview/flatlist components
-  // async scrollToElement(element: Detox.Element){
-  //   await waitFor(element).toBeVisible().whileElement(this.selector).scroll(100, 'down');
-  // }
 }
