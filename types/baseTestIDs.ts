ad6c542ed2145a7ce192502acee14fd9eb1c1d1f
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-05-02 10:08:30 +0300

    removing handleModal function

--- types/baseTestIDs.ts
+++
@@ -1,7 +0,0 @@
-import { ModalConfig } from "./modal";
-
-export const BASE_MODAL_IDS: ModalConfig[] = [
-  { id: "permission-modal", acceptButtonId: "allow", dismissButtonId: "deny" },
-  { id: "error-modal", dismissButtonId: "close" },
-  { id: "update-dialog", acceptButtonId: "update-now", dismissButtonId: "close" },
-];
