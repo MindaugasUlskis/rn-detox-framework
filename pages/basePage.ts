fafc310cc0f83b75fefaba547f10484c73519c59
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-05-02 10:08:30 +0300

    removing handleModal function

--- pages/basePage.ts
+++ pages/basePage.ts
@@ -1,48 +1,22 @@
 import { BaseComponent } from "../components/BaseComponent";
-import { BASE_MODAL_IDS } from "../types/baseTestIDs";
 import { expect } from "detox";
-import { ModalAction } from "../types/modal";
-import { logger } from "../utils/logger";
 
 export class BasePage {
   public popUpNotification: BaseComponent = new BaseComponent("loader-spinner");
   public loader: BaseComponent = new BaseComponent("pop-up-notification");
+  public errorModal: BaseComponent = new BaseComponent("error-modal");
+
+  // missing ButtonComponent -> should be implemented further task
+  // public closeErrorModalButton: ButtonComponent = new ButtonComponent("close")
 
   public async waitForLoaderToFinish(): Promise<void> {
     await expect(this.loader.getElement("id")).toBeVisible();
     await expect(this.loader.getElement("id")).not.toBeVisible();
   }
 
-  public async handleCommonModal(
-    action: ModalAction,
-    testId: string
-  ): Promise<void> {
-    const modalObject = Object.values(BASE_MODAL_IDS).find(
-      (mod) => mod.id === testId
-    );
-    if (!modalObject) {
-      const msg = `${testId} is not in the BASE_MODAL_IDS`;
-      logger.error(msg);
-      throw new Error();
-    }
-
-    const modal = new BaseComponent(testId);
-    await expect(modal.getElement("id")).toBeVisible();
-
-    if (action === "isVisible") return;
-
-    const modalButtonId =
-      action === "accept"
-        ? modalObject?.acceptButtonId
-        : modalObject?.dismissButtonId;
-
-    if (!modalButtonId) {
-      const msg = `${testId} modal does not support this action: ${action}`;
-      logger.error(msg);
-      throw new Error();
-    }
-
-    await new BaseComponent(modalButtonId).tap("id");
-    await expect(modal.getElement("id")).not.toBeVisible();
+  public async closeErrorModal(): Promise<void> {
+    await expect(this.errorModal.getElement("id")).toBeVisible();
+    //await expect (this.closeErrorModalButton.getElement('id')).tap()
+    await expect(this.errorModal.getElement("id")).not.toBeVisible();
   }
 }
--- types/modal.ts
+++
@@ -1,7 +0,0 @@
-export type ModalAction = "accept" | "dismiss" | "isVisible";
-
-export type ModalConfig = {
-  id: string;
-  acceptButtonId?: string;
-  dismissButtonId?: string;
-};
