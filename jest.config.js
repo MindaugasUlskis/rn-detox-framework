2bbe77d898ba6f4814959418b5ef0191b6a63b33
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-05-05 16:54:01 +0300

    fix bad js config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: "ts-jest",
-  rootDir: "..",
-  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'detox/runners/jest/testEnvironment',
+  testMatch: ['<rootDir>/tests/**/*.test.ts'],
   testTimeout: 120000,
   maxWorkers: 1,
-  globalSetup: "detox/runners/jest/globalSetup",
-  globalTeardown: "detox/runners/jest/globalTeardown",
-  reporters: ["detox/runners/jest/reporter"],
-  testEnvironment: "detox/runners/jest/testEnvironment",
+  rootDir: '.',
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
+  reporters: ['detox/runners/jest/reporter'],
   verbose: true,
 };
