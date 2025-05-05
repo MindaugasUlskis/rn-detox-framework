943b4f92d1686e4ad97ccc54bf4b553aaecec3a1
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-05-05 10:18:35 +0300

    sanity test

---
+++ e2e/demo.test.ts
@@ -0,0 +1,10 @@
+import { expect } from "detox";
+describe("Sanity", () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  it("should show the main screen", async () => {
+    await expect(element(by.text("WEBDRIVER"))).toBeVisible();
+  });
+});
