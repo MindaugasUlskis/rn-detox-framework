3fc9cad02fcbe53dc9cb2e05ed5d468c031b0618
Mindaugas Ulskis <mindaugas.ulskis@gmail.com>
2025-05-09 15:54:17 +0300

    more rules for eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,28 @@ export default tseslint.config(
       "@stylistic/ts/comma-dangle": ["error", "always-multiline"],
       "@stylistic/ts/object-curly-spacing": ["error", "always"],
       "@stylistic/ts/space-before-function-paren": ["error", "never"],
+      "@stylistic/ts/member-delimiter-style": [
+        "error",
+        {
+          multiline: { delimiter: "semi", requireLast: true },
+          singleline: { delimiter: "semi", requireLast: false },
+        },
+      ],
+      "@stylistic/ts/space-infix-ops": "error",
+      "@stylistic/ts/type-annotation-spacing": "error",
+
+      "new-cap": ["error", { newIsCap: true, capIsNew: false }],
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "no-empty-function": "warn",
+      "eqeqeq": ["error", "always"],
+      "curly": "error",
+      "no-implicit-coercion": "warn",
+
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      "@typescript-eslint/consistent-type-imports": "error",
     },
   },
 );
